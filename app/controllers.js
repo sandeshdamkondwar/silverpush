959f378d5cf9158a902a5c280ffc388082c1103c
Sandesh <sandeshdamkondwar@gmail.com>
2015-01-22 22:34:06 +0530

    Avoiding css hacks to fetch profile images

--- app/controllers.js
+++ app/controllers.js
@@ -4,6 +4,7 @@ function TeamController ($scope, $http) {
     $http.get("app/data/teamData.json").success(function(data) {
         $scope.team = data;
         $scope.query = '';
+        $scope.profileImagesPath = "../img/team-img/";
         $scope.empOrderBy = 'name';
     });
 }
